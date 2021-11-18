764a48e205520b0069253942d13e0204f3d62747
Дмитрий Вокуев <vokuev1996@inbox.ru>
2021-11-18 20:54:19 +0300

    Добавьте файлы проекта.

---
+++ 1_Task5_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Task5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float s = 0;
+            const int n = 7;
+            int[] array = new int[n];
+            
+            for (int i=0;i<n;i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                s += array[i];
+            }
+            Console.WriteLine("Среднее арифметическое чисел равно {0:f2}", s / n);
+            Console.ReadKey();
+        }
+    }
+}
